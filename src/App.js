56cfab89786d184f1379ed7744c0f56d667f5b54
mamadx3 <you@example.com>
2023-07-17 14:19:51 +0330

    delete loading

--- src/App.js
+++ src/App.js
@@ -20,29 +20,8 @@ const router = createBrowserRouter([
   }
 ]);
 const App = () => {
-  const { loadingPage , houses} = useContext(houseList);
-  if (loadingPage) {
-    return (
-      <>
-        <div className="containerr">
-          <svg width="100%" height="100" viewBox="-1 -1 22 22">
-            <path style={{ '--order': 7 }} className="eight e-8" />
-            <path style={{ '--order': 6 }} className="eight e-7" />
-            <path style={{ '--order': 5 }} className="eight e-6" />
-            <path style={{ '--order': 4 }} className="eight e-5" />
-            <path style={{ '--order': 3 }} className="eight e-4" />
-            <path style={{ '--order': 2 }} className="eight e-3" />
-            <path style={{ '--order': 1 }} className="eight e-2" />
-            <path style={{ '--order': 0 }} className="eight e-1" />
-          </svg>
-        </div>
-      </>
-    )
-  }
-
-
   return (
-    <div className='bg-white container mx-auto px-0 shdow-app'>
+    <div className='bg-white container mx-auto px-0 shdow-app '>
       <Header />
       <RouterProvider router={router} />
       <Footer />
